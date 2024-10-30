19bc47431d416e784d1c750956248bfbb85ec9ea
woshicezai <behappys@sina.com>
2024-10-30 22:12:20 +0800

    feat: 解决build报错的问题

--- src/pkg_web/src/App.vue
+++ src/pkg_web/src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="container">
-    <h1>Hello World Web</h1>
+  <div>
+    <h1>Web App</h1>
   </div>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-h1 {
-  color: #1890ff;
-}
-</style> 
+<script setup lang="ts">
+// Component logic here
+</script>
