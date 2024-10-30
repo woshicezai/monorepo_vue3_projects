6e6131821c091ebfc26b849bb05f5a01023551db
woshicezai <behappys@sina.com>
2024-10-30 21:36:22 +0800

    feat: 抽出公共的vite base 配置

--- src/pkg_web/vite.config.ts
+++ src/pkg_web/vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, mergeConfig } from "vite";
+import baseConfig from "../../vite.config.base";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3003
-  }
-}) 
+export default defineConfig((configEnv) => {
+  const config = {
+    // 子项目特定配置
+    server: {
+      port: 3003, // 覆盖基础配置的端口
+    },
+    build: {
+      outDir: "dist",
+    },
+    // 其他特定配置...
+  };
+
+  return mergeConfig(baseConfig, config);
+});
---
+++ vite.config.base.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+// 通用配置
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@libs": path.resolve(__dirname, "./src/libs"),
+    },
+  },
+  build: {
+    // 通用构建配置
+    target: "es2015",
+    minify: "terser",
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "vue-vendor": ["vue", "pinia"],
+        },
+      },
+    },
+  },
+  // 通用开发服务器配置
+  server: {
+    port: 3000,
+    cors: true,
+    open: true,
+  },
+});
