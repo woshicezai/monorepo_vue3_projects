c0d878fcbb73f2ab8d8fc03252161f09dde51750
woshicezai <behappys@sina.com>
2024-11-01 18:06:57 +0800

    feat:xx

---
+++ src/pkg_money/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Hello World Money</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  color: #1890ff;
+}
+</style> 
